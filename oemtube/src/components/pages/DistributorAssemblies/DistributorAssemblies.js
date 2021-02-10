bdb36c976e1479b0f9ab86df45d29998183fef1d
Michele Rawlins <shellbop@att.net>
2021-02-10 10:44:27 -0600

    adding product lines

---
+++ oemtube/src/components/pages/DistributorAssemblies/DistributorAssemblies.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import distributor14 from '../../../photos/Distributor-14-line.jpg';
+import distributor40 from '../../../photos/Distributor-40-line.jpg';
+import distributorClose from '../../../photos/Distributor-close-up-Edit-1.jpg';
+import distributorSmall from '../../../photos/Small-Distributor-Assembly.jpg';
+
+import { Link } from 'react-router-dom'
+import './DistributorAssemblies.scss';
+
+class DistributorAssemblies extends React.Component {
+  state = {
+   DistributorAssemblies:{},
+    flavicon: [],
+  }
+
+  render() {
+    const { DistributorAssemblies } = this.state;
+   
+    return (
+      <div className="distributorAssembliesSections">
+        <img className="distributorAssemblies" id="distributorAssemblies-pic"src={distributor14} />
+        <p className="distributorAssemblies" id="distributor-text">OEM is pleased to announce the introduction of brazed distributor assemblies targeted at the HVAC/R market.</p>
+        <img className="distributorAssemblies" id="distributorAssemblies-pic"src={distributor40} />
+        <p className="distributorAssemblies" id="distributor-text">Utilizing proprietary brazing and testing processes, we will manufacture leak free distributor assemblies that meet our customer’s specification.  Our process allows for the prefabrication of complex bent distributor tubes.  </p>
+        <img className="distributorAssemblies" id="distributorAssemblies-pic"src={distributorClose} />
+        <p className="distributorAssemblies"id="distributor-text">Braze rings, installed on distributor tubes prior to braze operation, offer precise amount of filler material with repeatable results.  The assembly is purged with inert gas during the braze operation.  Complete assembly can be tested to verify flow and leak free production.</p>
+        <img className="distributorAssemblies" id="distributorAssemblies-pic"src={distributorSmall} />
+      </div>
+    );
+  }
+}
+
+  export default DistributorAssemblies;
--- oemtube/src/components/pages/Home/Home.js
+++ oemtube/src/components/pages/Home/Home.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import MyCarousel from '../../../components/shared/MyCarousel/MyCarousel';
 import favicon from '../../../photos/favicon.jpg';
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './Home.scss';
+import { NavLink as RRNavLink } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
 
 // import {
 //   Card, CardImg, CardTitle,CardBody, 
@@ -26,7 +37,7 @@ class Home extends React.Component {
   <h1 className="typewriter">Let us Fabricate your Future</h1>
   <article>
     <p className="intro">Explore our product lines.  Click the button below to begin your journey.</p>
-    <button className="intro-button" a href="#products">Browse Products</button>
+    <Link to='/products'><button className="intro-button" to='/products'>Browse Products</button></Link>
   </article>
 </section>
 
