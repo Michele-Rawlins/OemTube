3104a68c2dbb0db725f435e623d8e1edc13e5d00
Michele Rawlins <shellbop@att.net>
2021-03-04 10:59:34 -0600

    styling

--- oemtube/src/components/pages/Products/Products.js
+++ oemtube/src/components/pages/Products/Products.js
@@ -3,6 +3,7 @@ import headerPic from '../../../photos/Collage-for-home-screen-1.jpg';
 import './Products.scss';
 import { Link } from 'react-router-dom'
 import { NavLink as RRNavLink } from 'react-router-dom';
+import distributor40 from '../../../photos/Distributor-40-line.jpg';
 import {
   Collapse,
   Navbar,
@@ -11,6 +12,11 @@ import {
   Nav,
   NavItem,
   NavLink,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
 } from 'reactstrap';
 
 class Products extends React.Component {
@@ -25,7 +31,8 @@ class Products extends React.Component {
     return (
         <div className="products">
           <div className="sidebar">
-      <img className="column-3" src={headerPic}></img>
+      <img className="column-8" id="headerpic"src={headerPic}></img>
+      <div className="links">
           <Link className="links-products"to='/distributor'>Distributor Assemblies</Link>
           <br></br>
           <Link className="links-products"to='/serpentine-helical-coils'>Serpentine & Helical Coils</Link>
@@ -38,11 +45,29 @@ class Products extends React.Component {
           <br></br>
           <Link className="links-products"to='/tube-cut'>Tube Cut to Length</Link>          
           <br></br>
-            
+          </div>   
       </div>
          <div className="company-intro article">
         <p className="product-intro">OEM Tube is focused on providing high quality fabricated tubing to the HVAC and Refrigeration Industries.  We supply custom fabrication with copper, brass, aluminum and stainless steel.  Processes range from simple cut to length parts to complex brazed assemblies to fit your specific design requirements.  OEM Tube offers a high level of Quality and Customer Service . We believe in growth by investing in state of the art equipment, expanding our capabilities and supplier relationships globally. We are the source, you should consider first.</p>
       </div>
+<div className="product-cards">
+
+<Card className="p-2 col-example text-center d-flex col-3 dashCard">
+          <CardTitle tag="h4">Where are we?</CardTitle>
+          <CardImg className="cardPic"src={distributor40} alt="Card image cap" />
+          <CardBody>
+          <Link to='/distributor'>Distributors</Link>
+      </CardBody>
+      </Card>
+</div>
+
+
+
+
+
+
+
+      
       </div>
     );
 
