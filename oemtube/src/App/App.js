c3b91242a1c1675a66c3475e4216385c1d542036
Michele Rawlins <shellbop@att.net>
2021-04-27 14:23:20 -0500

    added quote files

--- oemtube/src/App/App.js
+++ oemtube/src/App/App.js
@@ -43,6 +43,7 @@ render() {
               <Route path='/products' component={Products} />
               <Route path='/aboutus' component={AboutUs} />
               <Route path='/contactUs' component={ContactUs} />
+              <Route path='/requestquote' component={RequestQuote} />
               <Redirect from="*" to='/home'/>
               </Switch>
               <MyFooter />
