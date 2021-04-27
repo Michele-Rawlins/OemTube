ca317bb6a1edb21a2eb1c8ba62b7e9caf0e80fef
Michele Rawlins <shellbop@att.net>
2021-04-27 12:07:46 -0500

    added styling and changing emailjs function

--- oemtube/src/components/pages/DistributorAssemblies/DistributorAssemblies.js
+++ oemtube/src/components/pages/DistributorAssemblies/DistributorAssemblies.js
@@ -3,7 +3,7 @@ import distributor14 from '../../../photos/Distributor-14-line.jpg';
 import distributor40 from '../../../photos/Distributor-40-line.jpg';
 import distributorClose from '../../../photos/Distributor-close-up-Edit-1.jpg';
 import distributorSmall from '../../../photos/Small-Distributor-Assembly.jpg';
-
+import distributor from '../../../photos/distributor.jpg';
 import { Link } from 'react-router-dom'
 import './DistributorAssemblies.scss';
 
@@ -19,19 +19,19 @@ class DistributorAssemblies extends React.Component {
     return (
       <div className="distributorAssembliesSections">
         <div className = "title">
-               <h1 classNme="title">Distributor Assemblies</h1>
-               <a className = "home-return" href='/home'> Back Home</a>
-      <a className = "product-return"href='/products'> Back to Products</a>
-      <a className = "quote-request"href='/requestquote'> Request A Quote</a>
-      </div>
+              <h1 className="title">Distributor Assemblies</h1>
+              <a className = "home-return" href='/home'> Back Home</a>
+              <a className = "product-return"href='/products'> Back to Products</a>
+              <a className = "quote-request"href='/requestquote'> Request A Quote</a>
+        </div>
       <div className="distributorAssemblies">
-        <img className="distributorAssemblies" id="distributorAssemblies-pic"src={distributor14} />
-        <p className="distributorAssemblies" id="distributor-text">OEM is pleased to announce the introduction of brazed distributor assemblies targeted at the HVAC/R market.</p>
-        <img className="distributorAssemblies" id="distributorAssemblies-pic"src={distributor40} />
-        <p className="distributorAssemblies" id="distributor-text">Utilizing proprietary brazing and testing processes, we manufacture leak free distributor assemblies that meet our customer’s specification.  Our process allows for the prefabrication of complex bent distributor tubes.  </p>
-        {/* <img className="distributorAssemblies" id="distributorAssemblies-pic"src={distributorClose} /> */}
-        <p className="distributorAssemblies"id="distributor-text">Braze rings, installed on distributor tubes prior to braze operation, offer precise amount of filler material with repeatable results.  The assembly is purged with inert gas during the braze operation.  Complete assembly can be tested to verify flow and leak free production.</p>
-        <img className="distributorAssemblies" id="distributorAssemblies-pic"src={distributorSmall} />
+      <p className="distributorAssemblies" id="distributor-text">OEM is pleased to announce the introduction of brazed distributor assemblies targeted at the HVAC/R market.
+          Utilizing proprietary brazing and testing processes, we manufacture leak free distributor assemblies that meet our customer’s specification.  Our process allows for the prefabrication of complex bent distributor tubes.  </p>
+      <img className="distributorAssemblies" id="distributorAssemblies-pic"src={distributor14} />
+      <img className="distributorAssemblies" id="distributorAssemblies-pic"src={distributor40} />
+      <p className="distributorAssemblies"id="distributor-text">Braze rings, installed on distributor tubes prior to braze operation, offer precise amount of filler material with repeatable results.  The assembly is purged with inert gas during the braze operation.  Complete assembly can be tested to verify flow and leak free production.</p>
+      <img className="distributorAssemblies" id="distributorAssemblies-pic"src={distributorSmall} />
+      <img className="distributorAssemblies" id="distributorAssemblies-pic"src={distributor} />
       </div>
       </div>
     );
